9cb375e3c22f0f1faf323efc4c02598b090046ca
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-20 20:25:08 +0700

    Spring boot project: set up

---
+++ src/main/java/org/example/jobsevice/JobSeviceApplication.java
@@ -0,0 +1,15 @@
+package org.example.jobsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeviceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JobSeviceApplication.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/jobsevice/JobSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobsevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
