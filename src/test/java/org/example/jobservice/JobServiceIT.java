e758a12800e3d020442fe6f0ba337fe08643aeb6
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-21 09:03:44 +0700

    TestContainers: Basic tests

---
+++ src/test/java/org/example/jobservice/JobServiceIT.java
@@ -0,0 +1,54 @@
+package org.example.jobservice;
+
+import org.example.jobservice.dto.JobDto;
+import org.example.jobservice.generic.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Set;
+
+@SpringBootTest
+@AutoConfigureWebTestClient(timeout = "10000")
+class JobServiceIT extends BaseTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void allJobsTest() {
+        this.client.get()
+                .uri("/job/all")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .consumeWith(e -> System.out.println(new String(e.getResponseBody())))
+                .jsonPath("$").isNotEmpty();
+    }
+
+    @Test
+    public void searchBySkillsTest() {
+        this.client.get()
+                .uri("/job/search?skills=java")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.size()").isEqualTo(3);
+    }
+
+    @Test
+    public void postJobTest() {
+        var dto = JobDto.create(null, "k8s engr", "google", Set.of("k8s"), 200000, true, null);
+        this.client.post()
+                .uri("/job")
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("k8s engr");
+    }
+
+}
